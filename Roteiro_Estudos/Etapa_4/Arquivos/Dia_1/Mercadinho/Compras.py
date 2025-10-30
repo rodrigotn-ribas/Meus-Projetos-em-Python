491d2281a49c3927676f0811686c0d7181c16417
Rodrigo <rodrigotn.ribas@gmail.com>
2025-10-30 16:12:46 -0300

    clean_code

--- Roteiro_Estudos/Etapa_4/Arquivos/Dia_1/Mercadinho/Compras.py
+++ Roteiro_Estudos/Etapa_4/Arquivos/Dia_1/Mercadinho/Compras.py
@@ -4,6 +4,7 @@ diretorio = 'Roteiro_Estudos/Etapa_4/Arquivos/Dia_1/Mercadinho/'
 
 
 def escolha_lista():
+    print("Escolha uma lista: ")
     for file in os.listdir(diretorio):
         # Verifica se o item é um arquivo e se termina com .txt
         if file.endswith(".txt"):
@@ -20,6 +21,7 @@ def display():
     print("=" * 30)
 
 def escreve_item():
+    print("Escreva um item: ")
     item = input("")
     lista.append(item)
     for items in lista:
@@ -33,7 +35,7 @@ while True:
     
     if opcao == 0:
         break
-    
+
     # Cadastrar Items
     if opcao == 1:
         escolha_lista()
@@ -42,10 +44,6 @@ while True:
         arquivo = open(f"{diretorio}{escolha_arquivo}", formato_arquivo)
         escreve_item()
         
-        continuar = input("deseja continuar(s/n): ")
-        if continuar.lower() == "n":
-           continue
-       
     # Visualizar Listas   
     if opcao == 2:
         escolha_lista()
@@ -53,7 +51,7 @@ while True:
         if os.path.exists(f"{diretorio}{escolha_arquivo}"):
             formato_arquivo = "r"
             arquivo = open(f"{diretorio}{escolha_arquivo}", formato_arquivo)
-            print(arquivo.read())
+            print(f"\n{arquivo.read()}")
             arquivo.close()
         else:
             print("nao existe")
@@ -61,10 +59,9 @@ while True:
     # Criar novas Listas
     if opcao == 3:
         formato_arquivo = "w"
-        nome_arquivo = input("digite o nome da sua lista: ")
-        nome_arquivo += ".txt"
+        escolha_arquivo = input("Digite o nome da sua lista: ") + ".txt"
         # Vai criar um novo arquivo com um novo nome
-        arquivo = open(f"{diretorio}{nome_arquivo}", formato_arquivo)
+        arquivo = open(f"{diretorio}{escolha_arquivo}", formato_arquivo)
         escreve_item()
         
     # Excluir Listas
@@ -75,5 +72,4 @@ while True:
             os.remove(f"{diretorio}{escolha_arquivo}")
         else:
             print("Arquivo n existe") 
-        
         

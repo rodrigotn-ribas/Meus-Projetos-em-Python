21600cf85eb3b499ec4f1282b584c6e7aaf1c252
rodrigotn-ribas <rodrigotn.ribas@gmail.com>
2025-11-07 09:01:11 -0300

    pandas e numpy

--- Roteiro_Estudos/Etapa_6/Ex01.py
+++ Roteiro_Estudos/Etapa_6/Ex01.py
@@ -16,9 +16,22 @@ tabela_notas_df = pd.read_csv('Roteiro_Estudos/Etapa_6/notas_e_medias.csv')
 
 # tabela_alunos_df.loc[:, "Media"] = 0 // cria uma coluna com os valores zerados
 
-tabela_final_df = pd.concat([tabela_alunos_df, tabela_notas_df], axis=1)
+tabela_final_df = pd.concat([tabela_alunos_df, tabela_notas_df], axis=1) # axis = 1 : coluna; axis = 0 : linha;
 
-tabela_final_df = tabela_final_df.drop('media', axis=1)
+# tabela_final_df = tabela_final_df.drop('media', axis=1) // retira a coluna media
 
+# tabela_final_df = tabela_final_df.dropna(how='all', axis=1) // exclui uma coluna ou uma linha
 
-print(tabela_final_df)
+# tabela_final_df = tabela_final_df.dropna(how='all', axis=1) // exclui linhas com ao menos um valor vazio 
+
+# tabela_final_df['media'] = tabela_final_df['media'].fillna(tabela_alunos_df['media'].mean) // preenche os valores vazios com a media das medias
+
+# tabela_final_df = tabela_alunos_df.ffill // preenche os valores NAN com o primeiro valor de cima
+
+# cursos = tabela_final_df['curso'].value_counts() // mostra valor por cada curso
+
+# media_curso = tabela_final_df[['curso', 'media']].groupby('curso').mean() // mostra a media de cada curso
+
+nota_max = tabela_final_df.max()
+
+print(nota_max)
